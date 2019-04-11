77893e942e587b307d3718c6027a1e77e1022158
caoxinyu <caoxinyu@gmail.com>
2019-04-11 15:13:25 +0800

    update device type script

--- src/main/java/com/leaniot/api/stomp/AbstractEventSubscriber.java
+++ src/main/java/com/leaniot/api/stomp/AbstractEventSubscriber.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 import org.springframework.core.ParameterizedTypeReference;
 
-import com.leaniot.exception.ValueException;
-
 public abstract class AbstractEventSubscriber implements EventSubscriber{
 	protected Map<String, Object> types = new HashMap<String, Object>();
 	
@@ -14,12 +12,6 @@ public abstract class AbstractEventSubscriber implements EventSubscriber{
 		
 	}
 	
-	protected void checkType(Map<String, Object> type) {
-		for (Map.Entry<String, Object> entry : type.entrySet()) {
-			if(!(entry.getValue() instanceof Class<?>) &&  !(entry.getValue() instanceof ParameterizedTypeReference<?>) )
-					throw new ValueException("must be Class<?> or ParameterizedTypeReference<?>");
-		}
-	}
 	protected  void addType(String name, Class<?> type) {
 		types.put(name, type);
 	}
